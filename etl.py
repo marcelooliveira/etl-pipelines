5736af6dd82b9fdaedea8240dc649b354a443055
marcelooliveira <mclricardo@gmail.com>
2025-04-26 10:47:22 -0300

    github actions

--- etl.py
+++ etl.py
@@ -3,13 +3,10 @@ import pandas as pd
 import pandera as pa
 import sqlite3
 from sqlalchemy import create_engine
-from prefect import flow, task, get_run_logger
 import os
 
-@task(retries=3, retry_delay_seconds=5)
 def extract_data():
-    logger = get_run_logger()
-    logger.info("Fetching cryptocurrency data from CoinGecko API")
+    print("Fetching cryptocurrency data from CoinGecko API")
     url = "https://api.coingecko.com/api/v3/coins/markets"
     params = {
         "vs_currency": "usd",
@@ -20,13 +17,11 @@ def extract_data():
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
-    logger.info("Data extraction successful")
+    print("Data extraction successful")
     return response.json()
 
-@task
 def validate_data(df):
-    logger = get_run_logger()
-    logger.info("Validating data schema")
+    print("Validating data schema")
     schema = pa.DataFrameSchema({
         "Id": pa.Column(str),
         "Symbol": pa.Column(str),
@@ -37,30 +32,25 @@ def validate_data(df):
     })
     return schema.validate(df)
 
-@task
 def transform_data(data):
-    logger = get_run_logger()
-    logger.info("Transforming data")
+    print("Transforming data")
     df = pd.json_normalize(data)
     df = df[["id", "symbol", "name", "current_price", "market_cap", "total_volume"]]
     df.columns = [col.replace("_", " ").title() for col in df.columns]
-    logger.info("Data transformation complete")
+    print("Data transformation complete")
     return df
 
-@task
 def load_data(df):
-    logger = get_run_logger()
-    logger.info("Loading data into SQLite database")
+    print("Loading data into SQLite database")
     db_path = "data/crypto.db"  # Define the database path
     db_dir = os.path.dirname(db_path) #get the directory
     if db_dir: # Check if the directory exists
         os.makedirs(db_dir, exist_ok=True)  # Create the directory if it doesn't exist
-        logger.info(f"Database directory created: {db_dir}") #log
+        print(f"Database directory created: {db_dir}") #log
     engine = create_engine(f"sqlite:///{db_path}") #use the full path
     df.to_sql("cryptocurrencies", engine, if_exists="replace", index=False)
-    logger.info("Data successfully loaded")
+    print("Data successfully loaded")
 
-@flow(name="crypto-etl-pipeline")
 def etl_pipeline():
     raw_data = extract_data()
     cleaned_data = transform_data(raw_data)
@@ -68,5 +58,4 @@ def etl_pipeline():
     load_data(validated_data)
 
 if __name__ == "__main__":
-    # Run the flow
     etl_pipeline()
